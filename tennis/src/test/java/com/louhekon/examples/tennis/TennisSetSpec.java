47d915d83a20444f2dc39d190d4bcb61bc0dd284
Aki Salmi <aki.salmi@iki.fi>
2011-08-14 13:56:30 +0300

    some refactoring for tests

--- tennis/src/test/java/com/louhekon/examples/tennis/TennisSetSpec.java
+++ tennis/src/test/java/com/louhekon/examples/tennis/TennisSetSpec.java
@@ -8,26 +8,21 @@ import org.junit.runner.RunWith;
 
 @RunWith(JDaveRunner.class)
 public class TennisSetSpec extends Specification<TennisSet> {
-	
-	private final Player PLAYER_1 = PlayerSpec.PLAYER_1; 
-	private final Player PLAYER_2 = PlayerSpec.PLAYER_2; 
+
+	private final Player PLAYER_1 = PlayerSpec.PLAYER_1;
+	private final Player PLAYER_2 = PlayerSpec.PLAYER_2;
 	private final SetStatusObserver observer = mock(SetStatusObserver.class);
 	private Scorer scorer;
 	private final GameFactory factory = mock(GameFactory.class);
 	private final Game game = mock(Game.class);
-	
-	public class WithAny{
-		
+
+	public class WithAny {
+
 		private final Game gameNo2 = mock(Game.class, "AfterWinningOneGame");
 
-		public void shouldHaveZeroGamesForPlayers(){
-			specify(context.gamesFor(PLAYER_1), should.equal(0));
-			specify(context.gamesFor(PLAYER_2), should.equal(0));
-		}
-		
-		public TennisSet create(){
+		public TennisSet create() {
 			scorer = new SetScorer(PLAYER_1, PLAYER_2);
-			checking(new Expectations(){
+			checking(new Expectations() {
 				{
 					one(factory).newGame();
 					will(returnValue(game));
@@ -37,13 +32,18 @@ public class TennisSetSpec extends Specification<TennisSet> {
 			set.registerObserver(observer);
 			return set;
 		}
-		
-		public void shouldCreateANewGameWhenStartingANewSet(){
-			//Empty - already tested in initialization
+
+		public void shouldStartWithZeroGames() {
+			specify(context.gamesFor(PLAYER_1), should.equal(0));
+			specify(context.gamesFor(PLAYER_2), should.equal(0));
 		}
-		
-		public void shouldMarkPointToCorrectPlayerForTheCurrentGame(){
-			checking(new Expectations(){
+
+		public void shouldCreateANewGameWhenStartingANewSet() {
+			// Empty - already tested in initialization
+		}
+
+		public void shouldMarkPointToCorrectPlayerForTheCurrentGame() {
+			checking(new Expectations() {
 				{
 					one(game).addPointFor(PLAYER_1);
 				}
@@ -51,28 +51,40 @@ public class TennisSetSpec extends Specification<TennisSet> {
 			context.addPointFor(PLAYER_1);
 		}
 
-		public void shouldCreateNewGameWhenAPlayerWinsOne(){
-			checking(new Expectations(){
-				{
-					one(factory).newGame();
-					will(returnValue(gameNo2));
-					one(gameNo2).addPointFor(PLAYER_1);
-				}
-			});
+		public void shouldCreateNewGameWhenAPlayerWinsOne() {
+			expectGameToBeCreatedAfterWinningFirst(gameNo2);
 			context.gameBy(PLAYER_1);
+			expectTheCurrentGameIs(gameNo2);
 			context.addPointFor(PLAYER_1);
 		}
+
+		private void expectGameToBeCreatedAfterWinningFirst(final Game newGame) {
+			checking(new Expectations() {
+				{
+					one(factory).newGame();
+					will(returnValue(newGame));
+				}
+			});
+		}
+
+		private void expectTheCurrentGameIs(final Game newGame) {
+			checking(new Expectations() {
+				{
+					one(newGame).addPointFor(PLAYER_1);
+				}
+			});
+		}
 	}
-	
-	public class WhenWinningGames{
-		
-		public TennisSet create(){
+
+	public class WhenWinningGames {
+
+		public TennisSet create() {
 			scorer = mock(SetScorer.class);
 			return createTennisSet();
 		}
-		
-		public void shouldCalculatePointsWhenWinningOne(){
-			checking(new Expectations(){
+
+		public void shouldCalculatePointsWhenWinningOne() {
+			checking(new Expectations() {
 				{
 					one(scorer).addPointFor(PLAYER_1);
 					one(scorer).wonBy(PLAYER_1);
@@ -81,9 +93,9 @@ public class TennisSetSpec extends Specification<TennisSet> {
 			});
 			context.gameBy(PLAYER_1);
 		}
-		
-		public void shouldInformObserversForWinningASet(){			
-			checking( new Expectations(){
+
+		public void shouldInformObserversForWinningASet() {
+			checking(new Expectations() {
 				{
 					one(scorer).addPointFor(PLAYER_1);
 					one(scorer).wonBy(PLAYER_1);
@@ -102,7 +114,7 @@ public class TennisSetSpec extends Specification<TennisSet> {
 		}
 
 		private void withGameFactory() {
-			checking(new Expectations(){
+			checking(new Expectations() {
 				{
 					allowing(factory).newGame();
 				}
